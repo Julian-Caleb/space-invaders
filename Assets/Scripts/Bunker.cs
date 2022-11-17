7bbada321e4e66abb8dfa968596eeceba3dea8bd
Julian Caleb Simandjuntak <juliancaleb88@gmail.com>
2022-11-17 10:11:57 +0700

    Patch Notes 1.3.1
    - Bunker fix

--- Assets/Scripts/Bunker.cs
+++ Assets/Scripts/Bunker.cs
@@ -6,7 +6,7 @@ public class Bunker : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
 
         if (other.gameObject.layer == LayerMask.NameToLayer("Invader")) {
-            this.gameObject.SetActive(false);
+            Destroy(gameObject);
         } 
         
     }
